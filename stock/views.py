de69e52b2bc245b1236bb846ad63017d401a605e
Sumeyra Ozgur <smyrozgur@gmail.com>
2021-04-02 15:28:58 -0700

    - Add stock app - Read data from demo api - Start with first pages

---
+++ stock/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+import requests
+
+def overview(request):
+    response = requests.get('https://financialmodelingprep.com/api/v3/quote/AAPL?apikey=demo')
+    quote_data = response.json()[0]
+    return render(request, 'stock/overview/quote.html', {
+        'price': quote_data['price'],
+        'dayL': quote_data['dayLow'],
+        'dayH': quote_data['dayHigh'],
+        'yearH': quote_data['yearHigh'],
+        'yearL': quote_data['yearLow'],
+        'marketCap': quote_data['marketCap'],
+        'earnAnnounce': quote_data['earningsAnnouncement'],
+        'exchange': quote_data['exchange'],
+        'prePrice': quote_data['previousClose']
+
+        })
+
+
+def earning(request):
+    return render(request, 'stock/overview/earning.html', {'earning': 'hello world!'})
