1ce2a32e148440da6edc8fb6621dce232a9db3bf
Sumeyra Ozgur <smyrozgur@gmail.com>
2021-04-03 09:48:20 -0700

    Delete print code line

--- stock/templatetags/stock_tags.py
+++ stock/templatetags/stock_tags.py
@@ -8,7 +8,6 @@ def date_format(value, format_string):
     api_date_format = '%Y-%m-%dT%H:%M:%S.%f%z'
     try:    
         value = datetime.datetime.strptime(value, api_date_format)
-        print("sdafsdfasdfasfasdf",format_string, value)
         return value.strftime(format_string)
     except:
         return ''
